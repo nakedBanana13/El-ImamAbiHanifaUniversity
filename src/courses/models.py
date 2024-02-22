274a580e938e6601b937588aed06671b762061ac
Me13 <ahmad.turkmani2832000@hotmail.com>
2024-02-22 15:13:26 +0200

    Ordering the content

--- src/courses/models.py
+++ src/courses/models.py
@@ -2,6 +2,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.contrib.auth.models import User
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -37,9 +38,10 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 
 class Content(models.Model):
@@ -51,6 +53,11 @@ class Content(models.Model):
     object_id = models.PositiveIntegerField()  # to store the primary key of the related object
     item = GenericForeignKey('content_type', 'object_id')  # A GenericForeignKey field to the related object combining
     # the two previous fields
+
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
