eb78688cc8940ca04674244735d53563035a96b8
Me13 <ahmad.turkmani2832000@hotmail.com>
2024-02-23 16:05:25 +0200

    Course modules mangement

--- src/courses/models.py
+++ src/courses/models.py
@@ -3,7 +3,6 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
-
 from .fields import OrderField
 
 
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
+from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Course
+from .forms import ModuleFormSet
 
 
 class OwnerMixin(object):
@@ -19,7 +22,9 @@ class OwnerEditMixin(object):
 
 class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Course  # used for querySets(used by all views)
-    fields = ['subject', 'title', 'overview']  # The fields of the model to build the model form of the CreateView and UpdateView views
+    fields = ['subject', 'title', 'overview']  # The fields of the model to build the model form of the CreateView
+    # and UpdateView views
+
     success_url = reverse_lazy('manage_course_list')  # redirect the user after the form is successfully submitted
 
 
@@ -43,3 +48,30 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = 'courses.delete_course'
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    """handles adding, updating, and  deleting modules for a specific course"""
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course, data=data)
+
+    """attempts to delegate to a lowercase method  that matches the HTTP method used. A GET request is delegated to the 
+       get() method and a POST request to post() """
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course, id=pk, owner=request.user)
+        return super().dispatch(request, pk)
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course, 'formset': formset})
